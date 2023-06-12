dd23ce86524bbe521fa5ae067ef0cd6af74d5325
Rahul Parkar <rahul.a.parkar@gmail.com>
2023-06-12 22:12:52 +0100

    Fix VSCode Prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,17 @@
-module.exports =
-{
+module.exports = {
   root: true,
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint', 'jest', 'jest-extended'],
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint", "jest", "jest-extended"],
   extends: [
     "airbnb-base",
     "plugin:@typescript-eslint/recommended",
     "plugin:jest/recommended",
     "plugin:jest/style",
     "plugin:jest-extended/all",
-    "prettier"
+    "prettier",
   ],
-  "env": {
-    "jest": true,
-    "node": true
+  env: {
+    jest: true,
+    node: true,
   },
-}
-
+};
