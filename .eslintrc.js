7653c6ef48617b8adda60d763814926f0493bc8c
Rahul Parkar <rahul.a.parkar@gmail.com>
2023-06-12 22:06:15 +0100

    Add Prettier and TS linting config

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports =
+{
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'jest', 'jest-extended'],
+  extends: [
+    "airbnb-base",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:jest/recommended",
+    "plugin:jest/style",
+    "plugin:jest-extended/all",
+    "prettier"
+  ],
+  "env": {
+    "jest": true,
+    "node": true
+  },
+}
+
