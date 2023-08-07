a714ab02c095586d8f3b71e4ec054cf52f54a637
Rahul Parkar <rahul.a.parkar@gmail.com>
2023-08-07 21:39:05 +0100

    Update formatting

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -16,7 +16,7 @@ if (process.env.NODE_ENV === "production") {
   logger.add(
     new transports.Console({
       format: format.combine(format.colorize(), format.simple()),
-    })
+    }),
   );
 }
 
